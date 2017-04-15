207be2f3516453fca3eeb3e7fb18edc5ced3f81d
ognevlive <ognevlive@mail.ru>
2017-04-15 14:55:32 +0300

    Задание2, считывание

--- TV2/Source.cpp
+++ TV2/Source.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <math.h>
 #include <cmath>
+#include <fstream>
+#include <string>
 using namespace std;
 
 #define start (100)
@@ -35,13 +37,52 @@ void taskOne()
 	cout << "Xu^2 = " << Xu << endl;
 }
 
+enum zhanr {
+	sport, habr, nabokov
+};
+
+vector<int> read(zhanr t)
+{
+	int i;
+	ifstream fin;
+	string file, text;
+	vector<int> ABC(32);
+
+	if (t == sport)  fin.open("1.txt");
+	else if (t == habr) fin.open("2.txt");
+	else if (t == nabokov) fin.open("3.txt");
+
+	while (fin)
+	{
+		getline(fin, file);
+		text += file;
+	}
+
+	vector<unsigned char> unsText;
+	for (i = 0; i < text.size(); i++)
+		unsText.push_back(text[i]);
+
+	for (i = 0; i < unsText.size(); i++)
+		if (unsText[i] < 224 && unsText[i] > 191)
+			ABC[unsText[i] - 192]++;
+		else if (unsText[i] > 223 && unsText[i] < 256)
+			ABC[unsText[i] - 224]++;
+
+	fin.close();
+	return ABC;
+}
+
 void taskTwo()
 {
-
+	vector<vector<int>> ABC;
+	ABC.push_back(read(sport));
+	ABC.push_back(read(habr));
+	ABC.push_back(read(nabokov));
 }
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	taskOne();
 	taskTwo();
 	return 0;
